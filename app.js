fd08915d0661231bac17f17df2426e8ffefb140b
Dinesh Patel <dineshnpatel24@gmail.com>
2024-09-23 20:35:16 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let linkedln =document.querySelector(".linkedln");
+
+
+
+linkedln.addEventListener("click",()=>{
+console.log("btn was cliked");
+window.open("https://www.linkedin.com/in/dineshpatel24/" )
+}
+);
+
+
